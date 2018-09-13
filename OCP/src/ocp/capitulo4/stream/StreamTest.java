8ad0fbb90bff10f91ed570d3bcdfaedefbf91cc3
serrarenato <serra.renato@gmail.com>
2018-09-13 17:03:01 -0300

    Validate

--- OCP/src/ocp/capitulo4/stream/StreamTest.java
+++ OCP/src/ocp/capitulo4/stream/StreamTest.java
@@ -1,7 +1,9 @@
 package ocp.capitulo4.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -25,6 +27,19 @@ public class StreamTest {
 		System.out.println("Count: " + streamTest.count());
 		///////////////////// get the minimun
 		streamTest = Stream.of("teste", "teste2", "teste31");
-		System.out.println("Min value: " + streamTest.min((s1, s2) -> s1.length() - s2.length()).get()); // min return a Optional
+		System.out.println("Min value: " + streamTest.min((s1, s2) -> s1.length() - s2.length()).get()); // min return a
+																											// Optional
+		///// findAny
+		streamTest = Stream.of("teste", "teste2", "teste31");
+		System.out.println("FindAny:" + streamTest.findAny());
+		///Stream streamTest2 = Stream.generate(() -> "joao"); // infinite stream
+		//streamTest2.forEach(System.out::print);
+
+		// AnyMatch AllMatch NoneMatch
+		List<String> list1 = Arrays.asList("teste", "2", "teste3");
+		Predicate<String> predicate = l -> Character.isLetter(l.charAt(0));
+		System.out.println("AllMatch: " + list1.stream().allMatch(predicate));
+		System.out.println("AnyMatch: " + list1.stream().anyMatch(predicate));
+		System.out.println("NoneMatch: " + list1.stream().noneMatch(predicate));
 	}
 }
